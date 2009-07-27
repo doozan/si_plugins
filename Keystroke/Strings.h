8a888ffde9ea460112d36fd6fc8f725c88bd1692
Jeff Doozan <jeff@tcbmi.com>
2009-07-27 22:39:34 +0000

    Initial Checkin
    git-svn-id: http://svn.tcbmi.com/strokeit@11 8da159d4-87d2-4145-b014-9f08f8e1538a

---
+++ Keystroke/Strings.h
@@ -0,0 +1,19 @@
+// The plugin version.  This must match the version of the string files
+VERSION(.9.1)
+
+// Command names in dropdown box
+STR(KEYS)
+STR(HOTKEY)
+STR(PASS)
+
+// Command descriptions
+STR(KEYS_DESC)
+STR(HOTKEY_DESC)
+STR(PASS_DESC)
+
+STR(KEYS_HELP)
+STR(HOTKEY_LABEL)
+STR(PASS_LABEL)
+
+// Error Definitions
+STR(ERR_UNKNOWN_KEY)
