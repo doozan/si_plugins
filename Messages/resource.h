0e5267fe3632ee3027772acb55d5a35aa9216333
Jeff Doozan <jeff@tcbmi.com>
2009-07-27 22:32:35 +0000

    Initial Checkit
    git-svn-id: http://svn.tcbmi.com/strokeit@3 8da159d4-87d2-4145-b014-9f08f8e1538a

---
+++ Messages/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by msg.rc
+//
+#define DLG_CLASSMSG                    101
+#define ICON_MSG                        112
+#define IDC_MSG                         1006
+#define IDC_WPARAM                      1009
+#define IDC_ARGS                        1010
+#define IDC_LPARAM                      1010
+#define IDC_LPARAM_DESC                 1011
+#define IDC_WPARAM_DESC                 1012
+#define IDC_MSG_DESC                    1013
+#define IDC_CLASS                       1014
+#define IDC_RADIO_CURRENT               1020
+#define IDC_RADIO_CLASS                 1021
+#define IDC_WINDOW_FRAME                1022
+#define IDC_MESSAGE_FRAME               1023
+#define IDC_MESSAGES                    1024
+#define IDC_MESSAGE                     1024
+#define IDC_CHECK_PATTERN               1025
+#define IDC_RADIO_CAPTION               1026
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
