7d6e071bc24f37cb0cce1abee98ab63181890786
Jeff Doozan <jeff@tcbmi.com>
2009-07-27 22:32:39 +0000

    Initial Checkit
    git-svn-id: http://svn.tcbmi.com/strokeit@4 8da159d4-87d2-4145-b014-9f08f8e1538a

---
+++ Mouse/resource.h
@@ -0,0 +1,60 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Mouse.rc
+//
+#define IDD_CONFIG                      101
+#define DLG_NOPARAMS                    101
+#define DLG_KEYS                        101
+#define DLG_RUN                         101
+#define DLG_MOUSE                       101
+#define ICON_MAXRESTORE                 102
+#define DLG_PARAMS                      102
+#define IDC_COMMAND                     103
+#define ICON_MINIMIZE                   104
+#define ICON_HIDE                       105
+#define ICON_MOVE                       106
+#define ICON_SHOW                       107
+#define ICON_SIZE                       108
+#define ICON_KEYS                       110
+#define ICON_RUN                        111
+#define ICON_WEB                        112
+#define ICON_MAIL                       114
+#define ICON_DEMO                       114
+#define ICON_MOUSE                      114
+#define IDC_KEYS                        1005
+#define IDC_FILE                        1006
+#define IDC_BROWSE                      1008
+#define IDC_START                       1009
+#define IDC_ARGS                        1010
+#define IDC_ARGS_DESC                   1011
+#define IDC_START_DESC                  1012
+#define IDC_FILE_DESC                   1013
+#define IDC_RADIO1                      1014
+#define IDC_RADIO_START                 1014
+#define IDC_BUTTON1                     1015
+#define IDC_COMBO_BUTTON                1017
+#define IDC_ACTION                      1018
+#define IDC_RADIO_END                   1019
+#define IDC_COMBO_CLICK                 1020
+#define IDC_COMBO_ACTION                1020
+#define IDC_RADIO_EXACT                 1021
+#define IDC_EDIT_X                      1022
+#define IDC_EDIT_Y                      1023
+#define IDC_CHECK_RELATIVE              1024
+#define IDC_Y                           1025
+#define IDC_X                           1026
+#define IDC_BUTTON                      1027
+#define IDC_LOCATION                    1028
+#define IDC_MOUSEBUTTON                 1029
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
